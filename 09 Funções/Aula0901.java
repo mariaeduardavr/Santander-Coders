79af1c8cbb66bbbbcde9bdc6f1e79b0b435826d7
Maria Eduarda <107825119+mariaeduardavr@users.noreply.github.com>
2022-09-17 00:35:09 -0300

    Aula 09 - Funções

---
+++ 09 Funções/Aula0901.java
@@ -0,0 +1,13 @@
+public class Aula0901 {
+    public static void main(String[] args) {
+        String nome = "Duda";
+
+        saudacao(nome);
+
+    }
+
+    public static void saudacao(String nome) {
+        System.out.println("Hello, " + nome);
+    }
+
+}
---
+++ 09 Funções/Aula0902.java
@@ -0,0 +1,12 @@
+public class Aula0902 {
+    public static void main(String[] args) {
+        String nomeOriginal = "Duda";
+
+        saudacao(nomeOriginal);
+
+    }
+
+    public static void saudacao(String nomeParametro) {
+        System.out.println("Hello, " + nomeParametro);
+    }
+}
