4ea068b0baee240c0f0ed67e739c3be29df5ed04
Maria Eduarda <107825119+mariaeduardavr@users.noreply.github.com>
2022-09-17 00:20:02 -0300

    Aula 08 - Vetores

---
+++ 08 Vetores/Aula0804.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Aula0804 {
+    public static void main(String[] args) {
+
+    String[] letras = {"A", "B", "C", "D", "E"};
+
+    System.out.println(Arrays.toString(letras));
+
+    }
+
+}
