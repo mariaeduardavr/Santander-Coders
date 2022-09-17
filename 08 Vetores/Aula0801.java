6c05f248092d0780efef2a2428d74b6e4f7b49cd
Maria Eduarda <107825119+mariaeduardavr@users.noreply.github.com>
2022-09-17 11:56:17 -0300

    Aula 08 - Vetores

---
+++ 08 Vetores/Aula0801.java
@@ -0,0 +1,19 @@
+public class Aula0801 {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[5];
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+
+        System.out.println(numeros);
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+
+    }
+}
