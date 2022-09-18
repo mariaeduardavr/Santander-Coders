4e3c9854471e252907d6e454f7b64da691c90840
Maria Eduarda <107825119+mariaeduardavr@users.noreply.github.com>
2022-09-18 17:22:03 -0300

    Update ex005.java

--- Lista de Exercícios/ex005.java
+++ Lista de Exercícios/ex005.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class ex005 {
     public static void main(String[] args) {
 
         String[] promocao = {"banana", "melancia", "abacaxi", "uva", "laranja"};
         String[] listaDeCompras = new String[5];
-        int contador = 0;
 
         Scanner input = new Scanner(System.in);
 
@@ -15,7 +13,17 @@ public class ex005 {
             listaDeCompras[i] = input.nextLine();        
         }
 
-        System.out.println(Arrays.toString(listaDeCompras));
-
+        int contador = 0;
+        for (int j = 0; j < listaDeCompras.length; j++) {
+            for (int k = 0; k < promocao.length; k++ ){
+                if (listaDeCompras[j].equalsIgnoreCase(promocao[k])) {
+                    contador++;
+                }
+            }
+        }
+        System.out.println("As frutas em promoção são:");
+        System.out.println(promocao);
+        System.out.println("Na sua lista há " + contador + " frutas em promoção.");
+        System.out.println("Portanto, você terá " + contador * 5 + "% de desconto em seu pedido.");
     }
 }
